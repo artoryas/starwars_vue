35e6a689442829c0211b0f07167259e7369bdf14
Ернияз Досанов <yerniyaz.dossanov@innoforce.kz>
2020-04-26 20:27:26 +0600

    Info on main page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <h2 class="">Home page</h2>
-        <p>Википедия по вселенной Звездных войн</p>
+    <div class="center">
+        <h2>Welcome to StarWars!</h2>
+        <p class="flow-text">Краткая информация по вселенной звездных войн</p>
+        <p class="flow-text">Перейдите по вкладкам чтобы получить информацию</p>
+        <p class="flow-text">Создано с использованием Vue.js, MaterializeCSS, Webpack, Babel</p>
     </div>
 </template>
